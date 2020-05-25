d54fc997631c4c712785aa4baf587880a7b0f218
hfuong <holly.fuong@columbia.edu>
2020-05-25 18:55:38 -0400

    Initial commit to use 'requests' package to scrape Basketball Reference site for Twitter handles

---
+++ twitterhandles.py
@@ -0,0 +1,19 @@
+# Import relevant modules
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+
+# Get web page
+site = requests.get("https://www.basketball-reference.com/friv/twitter.html")
+
+# Print status code; 200 = successfully downloaded
+print(site)
+
+
+# Make BeautifulSoup object
+page = BeautifulSoup(site.content, features = "lxml")
+
+print(page.prettify())
+
+
