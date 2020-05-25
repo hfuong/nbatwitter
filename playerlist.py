315a323d53ccb67f1621b36d2be1f0ad8be3af0d
hfuong <holly.fuong@columbia.edu>
2020-05-25 19:08:24 -0400

    Use BeautifulSoup to print HTML content with nice formatting

--- playerlist.py
+++ playerlist.py
@@ -1,9 +1,20 @@
 # Import relevant modules
 import requests
+import lxml
+from bs4 import BeautifulSoup
+
 
 # Get web page
 site = requests.get("https://stats.nba.com/players/list/")
 
 # Print status code; 200 = successfully downloaded
 print(site)
+
+
+# Make BeautifulSoup object
+page = BeautifulSoup(site.content, features = "lxml")
+
+# Print HTML content with nice formatting
+print(page.prettify())
+
 
