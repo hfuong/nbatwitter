abb6710557b25b3e6ddd08c7c0e29fee205af646
hfuong <holly.fuong@columbia.edu>
2020-06-11 15:50:26 -0400

    Add dynamic scrolling from NBA's official list of players

--- pyscripts/playerlist.py
+++ pyscripts/playerlist.py
@@ -1,13 +1,33 @@
 # Import relevant modules
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 
 
 # Get web page with dynamic content that requires scrolling to load
 driver = webdriver.Chrome(executable_path = "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe")
-driver.get('https://nba.com/players/')
+driver.get("https://nba.com/players/")
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(5)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
 
 html = driver.page_source
+
+# Close browser window at end of session
+driver.quit()
 
 # Make BeautifulSoup object
 page = BeautifulSoup(html)
