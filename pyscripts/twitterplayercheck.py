72839511d976a35e0d203ca7603ae8d7a1094827
hfuong <holly.fuong@columbia.edu>
2020-09-08 19:45:54 -0400

    Initial commit of twitterplayercheck.py

--- pyscripts/twitterplayercheck.py
+++ pyscripts/twitterplayercheck.py
@@ -1,7 +1,12 @@
 # Import relevant modules
-import csv
+import pandas as pd
 
 # Load CSV files
-player = csv.reader(open('data/currentplayers.csv', 'r'))
-twitter = csv.reader(open('data/currenttwitter.csv', 'r'))
+player = pd.read_csv('data/currentplayers.csv')
+twitter = pd.read_csv('data/currenttwitter.csv')
 
+# Check that the current players excluded actually do not have Twitter
+# i.e. make sure it is not a spelling error between NBA & Basketball Reference
+noTwitter = set(player['player']) ^ set(twitter['currentplayer'])
+
+print(noTwitter)
