c822bb25db595c16e67ebd72dd9683cb1287c8d7
hfuong <holly.fuong@columbia.edu>
2020-09-17 17:51:39 -0400

    Create edge lists

--- followers_edge_list.py
+++ followers_edge_list.py
@@ -15,7 +15,28 @@ df = df.merge(players.rename(columns = {'handle': 'user1_name', 'currentplayer':
 df = df.merge(players.rename(columns = {'handle': 'user2_name', 'currentplayer': 'user2_player'}),
               on = 'user2_name', how = 'left')
 
-# Re-order columns, remove screen names
-df = df[['user1_player', 'following_user2', 'user2_player', 'following_user1']]
+# Create two df where columns are follower, following, and True/False, remove screen names
+df1 = df[['user1_player', 'user2_player', 'following_user2']]
+df2 = df[['user2_player', 'user1_player', 'following_user1']]
 
-print(df)
+# Rename columns
+df1.columns = ['user_follower', 'user_following', 'following']
+df2.columns = ['user_follower', 'user_following', 'following']
+
+# Concatenate into one dataframe
+edgeList = pd.concat([df1, df2], ignore_index = True)
+
+# Remove values where following = False (i.e., include only True values)
+edgeList = edgeList[edgeList['following']]
+
+# Remove following column
+edgeList = edgeList.drop(columns = ['following'])
+
+# Write edge list to CSV, without index
+edgeList.to_csv('data/followers_edgelist.csv', index = False)
+
+# Count number of True values (this value will match final number of rows in edge list)
+print('Estimated number of rows:', len(df[df['following_user2']]) + len(df[df['following_user1']]))
+
+# For checking purposes, print length of edge list (should match estimate)
+print('Total length of edge list:', len(edgeList))
